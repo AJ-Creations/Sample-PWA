7b3b9ae44bff2d841b7ef1c8f852b15c575daadd
Jishnu Raj <jishnuraj.rockz@gmail.com>
2018-05-11 20:38:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if(navigator.serviceWorker.controller){
   console.log('[PWA Logs] active service worker is found on this browser. no need to register');
 }
 else {
-  navigator.serviceWorker.register('/sw.js', {
+  navigator.serviceWorker.register('/Sample-PWA/sw.js', {
     scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
