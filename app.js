66534b30ac171c45012041713e8a0d3465f8d325
AJ-Creations <jishnuraj.rockz@gmail.com>
2018-05-11 18:30:30 +0530

    Version 0.1

---
+++ app.js
@@ -0,0 +1,11 @@
+//Register Service Worker
+if(navigator.serviceWorker.controller){
+  console.log('[PWA Logs] active service worker is found on this browser. no need to register');
+}
+else {
+  navigator.serviceWorker.register('/sw.js', {
+    scope: './'
+  }).then(function(reg) {
+    console.log('Service worker has been registered for scope:'+ reg.scope);
+  });
+}
